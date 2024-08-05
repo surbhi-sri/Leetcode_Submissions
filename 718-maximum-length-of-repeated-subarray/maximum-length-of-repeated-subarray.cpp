7a6745503f8b9315fa8c1896c13f250189ecd001
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2024-08-05 11:28:06 +0530

    Time: 115 ms (26.58%) | Memory: 88.3 MB (10.45%) - LeetSync

---
+++ 718-maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findLength(vector<int>& x, vector<int>& y) {
+        int n = x.size();
+        int m = y.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        int mx = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (x[i - 1] == y[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 0;
+                mx = max(mx, dp[i][j]);
+            }
+        }
+        return mx;
+    }
+};
