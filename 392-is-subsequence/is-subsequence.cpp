fab8c851c817042353412eba08ee666b38af7bfa
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2024-08-10 12:22:51 +0530

    Time: 3 ms (46.96%) | Memory: 7.8 MB (39.66%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -3,16 +3,11 @@ public:
     bool isSubsequence(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+       int j=0;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s[i - 1] == t[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
-            }
+        for (int i = 0; i < m; i++) {
+          if(s[j]==t[i]) j++;
         }
-        return (n == dp[n][m]);
+        return (n == j);
     }
 };
