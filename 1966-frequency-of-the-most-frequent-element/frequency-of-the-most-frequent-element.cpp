faa8625e973299169580fe813f634b3bc152d0c5
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2024-11-24 10:31:26 +0530

    Time: 47 ms (30.73%) | Memory: 105.7 MB (80.32%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end(), greater<>());
-        int mx_cnt=1;
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            int ele = nums[i];
-            int kn = k;
-            int count = 1;
-            for (int j = i + 1; j < n; j++) {
-                while (j<n && ele == nums[j]) {
-                    i++;
-                    j++;
-                    count++;
-                }
-                if(j == n) break;
-                 kn -= (ele - nums[j]);
-                if (kn < 0)
-                    break;
-                count++;
+        sort(nums.begin(), nums.end());
+        int left = 0;
+        long curr = 0;
+
+        for (int right = 0; right < n; right++) {
+            long target = nums[right];
+            curr += target;
+
+            if ((right - left + 1) * target - curr > k) {
+                curr -= nums[left];
+                left++;
             }
-            if(count > mx_cnt) mx_cnt=count;
         }
-        return mx_cnt;
+        return n-left;
     }
 };
