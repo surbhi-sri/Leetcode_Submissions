f762f936a40c889ad44e89c4aea82bc593e07d75
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2024-12-16 18:27:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (19.08%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -9,12 +9,12 @@ public:
             if (nums[mid] == target)
                 return mid;
             else if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target <= nums[mid])
+                if (nums[left] <= target && target < nums[mid])
                     right = mid - 1;
                 else
                     left = mid + 1;
             } else {
-                if (nums[mid] <= target && target <= nums[right])
+                if (nums[mid] < target && target <= nums[right])
                     left = mid + 1;
                 else
                     right = mid - 1;
