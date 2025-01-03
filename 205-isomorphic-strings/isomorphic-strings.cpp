90364652b32bdca3b477edf8121591536aa5b6f2
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2025-01-03 19:03:42 +0530

    Time: 4 ms (28.78%) | Memory: 9.4 MB (61.14%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -6,16 +6,13 @@ public:
         int n = s.size();
         if (n == 1)
             return 1;
-        for (int i = 0; i < n; i++) {
-            if (mp.find(s[i]) == mp.end() && mpt.find(t[i]) == mpt.end()) {
-                mp[s[i]] = t[i];
-                mpt[t[i]] = s[i];
-            } else if (mp.find(s[i]) != mp.end() || mpt.find(t[i]) != mpt.end()) {
+        for (int i = 0; i < n; i++){
+            if (mp.find(s[i]) != mp.end() || mpt.find(t[i]) != mpt.end()) {
                 if (mp[s[i]] != t[i] || mpt[t[i]] != s[i])
                     return 0;
-                else
-                    continue;
             }
+            mp[s[i]] = t[i];
+            mpt[t[i]] = s[i];
         }
         return 1;
     }
