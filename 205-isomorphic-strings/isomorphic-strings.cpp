7094b9d258a60783f9599ca8e8da26bd377a5cde
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2025-01-03 19:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (43.81%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char, char> mp;
-        map<char, char> mpt;
+        vector<int> last_sennS(256, -1);
+        vector<int> last_sennT(256, -1);
         int n = s.size();
-        if (n == 1)
-            return 1;
-        for (int i = 0; i < n; i++){
-            if (mp.find(s[i]) != mp.end() || mpt.find(t[i]) != mpt.end()) {
-                if (mp[s[i]] != t[i] || mpt[t[i]] != s[i])
-                    return 0;
-            }
-            mp[s[i]] = t[i];
-            mpt[t[i]] = s[i];
+
+        for (int i = 0; i < n; i++) {
+            if (last_sennS[s[i]] != last_sennT[t[i]])
+                return 0;
+            last_sennS[s[i]] = i;
+            last_sennT[t[i]] = i;
         }
         return 1;
     }
