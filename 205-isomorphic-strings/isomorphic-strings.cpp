24a4455ffc79d4358969d59c779cf7f273cad542
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2025-01-03 18:48:51 +0530

    Time: 6 ms (15.50%) | Memory: 9.3 MB (61.14%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        map<char, char> mp;
+        map<char, char> mpt;
+        int n = s.size();
+        if (n == 1)
+            return 1;
+        for (int i = 0; i < n; i++) {
+            if (mp.find(s[i]) == mp.end() && mpt.find(t[i]) == mpt.end()) {
+                mp[s[i]] = t[i];
+                mpt[t[i]] = s[i];
+            } else if (mp.find(s[i]) != mp.end() || mpt.find(t[i]) != mpt.end()) {
+                if (mp[s[i]] != t[i] || mpt[t[i]] != s[i])
+                    return 0;
+                else
+                    continue;
+            }
+        }
+        return 1;
+    }
+};
