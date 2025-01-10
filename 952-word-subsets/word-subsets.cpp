166ded038ebc4a88ecde0db9e030ccd6355c2f79
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2025-01-10 23:40:35 +0530

    Time: 40 ms (72.15%) | Memory: 75.5 MB (72.15%) - LeetSync

---
+++ 952-word-subsets/word-subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<char> freq(26, 0);
+        for (const string& word : words2) {
+            vector<char> mfreq(26, 0);
+            for (char ch : word)
+                mfreq[ch - 'a']++;
+            for (int i = 0; i < 26; i++)
+                freq[i] = max(freq[i], mfreq[i]);
+        }
+
+        vector<string> ans;
+
+        for (const string& word : words1) {
+            vector<char> mfreq(26, 0);
+            int flag = 1;
+            for (char ch : word)
+                mfreq[ch - 'a']++;
+
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] > mfreq[i])
+                    flag = 0;
+            }
+
+            if (flag)
+                ans.push_back(word);
+        }
+        return ans;
+    }
+};
