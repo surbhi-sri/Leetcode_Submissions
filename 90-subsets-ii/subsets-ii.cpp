53f0e67b3c50071e35895133c786fbe73c09b9fe
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2025-01-15 00:44:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (52.77%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void count(vector<int>& nums, int j, vector<int>& temp,
-               set<vector<int>>& ans) {
-        if (j >= nums.size()) {
-            ans.insert(temp);
-            return;
-        }
+    void count(vector<int>& nums, int i, vector<int>& temp,
+               vector<vector<int>>& ans) {
 
-        temp.push_back(nums[j]);
-        count(nums, j + 1, temp, ans);
-        temp.pop_back();
-        count(nums, j + 1, temp, ans);
+        ans.push_back(temp);
+
+        for (int j = i; j < nums.size(); j++) {
+            if (j > i && nums[j] == nums[j - 1])
+                continue;
+            temp.push_back(nums[j]);
+            count(nums, j + 1, temp, ans);
+            temp.pop_back();
+        }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         vector<int> temp;
         count(nums, 0, temp, ans);
-        return vector<vector<int>>(ans.begin(), ans.end());
+        return ans;
     }
 };
