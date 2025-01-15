a4d14c1f9234c96ccdd67e9bab111822b761585c
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2025-01-15 23:35:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (26.57%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -5,18 +5,18 @@ public:
         while (n > 0 && nums[n - 1] >= nums[n])
             n--;
         if (n == 0) {
-            sort(nums.begin(), nums.end());
+            reverse(nums.begin(), nums.end());
             return;
         }
-        int ind = n - 1, mn = 200, mni;
-        for (int i = n; i < nums.size(); i++) {
-            if (nums[i] > nums[ind] && nums[i] < mn) {
-                mn = nums[i];
-                mni = i;
+        int ind = n - 1;
+        for (int i = nums.size()-1; i >= ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums[ind], nums[i]);
+                break;
             }
         }
-        swap(nums[ind], nums[mni]);
-        sort(nums.begin()+ind+1, nums.end());
+
+        reverse(nums.begin() + ind + 1, nums.end());
         return;
     }
 };
