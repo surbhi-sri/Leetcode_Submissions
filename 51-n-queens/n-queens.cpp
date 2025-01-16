62f7b255a0e678b04982dec94998a83e39bf580e
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2025-01-16 21:57:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (88.14%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,46 +1,25 @@
 class Solution {
 public:
-    bool safe(int row, int col, vector<string>& temp, int n) {
-        int drow = row;
-        int dcol = col;
-        while (drow >= 0 && dcol >= 0) {
-            if (temp[drow][dcol] == 'Q')
-                return 0;
-            drow--;
-            dcol--;
-        }
-
-        drow = row;
-        dcol = col;
-        while (dcol >= 0) {
-            if (temp[drow][dcol] == 'Q')
-                return 0;
-            dcol--;
-        }
-
-        drow = row;
-        dcol = col;
-        while (drow < n && dcol >= 0) {
-            if (temp[drow][dcol] == 'Q')
-                return 0;
-            drow++;
-            dcol--;
-        }
-
-        return 1;
-    }
-
     void solve(int col, vector<string>& temp, vector<vector<string>>& ans,
-               int n) {
+               int n, vector<int> &back, vector<int> &upper_dig,
+               vector<int> &lower_dig) {
+
         if (col == n) {
             ans.push_back(temp);
             return;
         }
         for (int row = 0; row < n; row++) {
-            if (safe(row, col, temp, n)) {
+            if (back[row] == 0 && lower_dig[row + col] == 0 &&
+                upper_dig[n - 1 + col - row] == 0) {
                 temp[row][col] = 'Q';
-                solve(col + 1, temp, ans, n);
+                back[row] = 1;
+                lower_dig[row + col] = 1;
+                upper_dig[n - 1 + col - row] = 1;
+                solve(col+1, temp, ans, n, back, upper_dig, lower_dig);
                 temp[row][col] = '.';
+                back[row] = 0;
+                lower_dig[row + col] = 0;
+                upper_dig[n - 1 + col - row] = 0;
             }
         }
     }
@@ -48,7 +27,10 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> temp(n, string(n, '.'));
-        solve(0, temp, ans, n);
+        vector<int> back(n, 0);
+        vector<int> upper_dig(2 * n - 1, 0);
+        vector<int> lower_dig(2 * n - 1, 0);
+        solve(0, temp, ans, n, back, upper_dig, lower_dig);
         return ans;
     }
 };
