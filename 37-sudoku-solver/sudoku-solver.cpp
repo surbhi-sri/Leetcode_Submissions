b0dc0ae678b9af05153a345b7579a9126fbf2fc0
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2025-01-16 23:25:46 +0530

    Time: 352 ms (55.92%) | Memory: 8.8 MB (56.42%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -10,24 +10,25 @@ public:
    }
    
 
-    bool solve(vector<vector<char>>& board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if(board[i][j]=='.'){
-                    for(char c='1'; c<='9'; c++){
-                        if(isvalid(board, i, j, c)){
-                           board[i][j]=c;
-                           if(solve(board)==1) return 1;
-                           else  board[i][j]='.';
-                        }
-                    }return false;
-                }
-            }
+    bool solve(vector<vector<char>>& board, int i, int j) {
+  if(i==9) return true;
+    if(j==9) return solve(board, i+1, 0);
+    if(board[i][j] != '.') return solve(board, i, j+1);
+
+    for(char c='1'; c<='9'; c++)
+    {
+        if(isvalid(board, i, j, c))
+        {
+            board[i][j] = c;
+            if(solve(board, i, j+1)) return true;
+            board[i][j] = '.';
         }
-        return true;
+    }
+        
+    return false;
     }
 
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        solve(board, 0, 0);
     }
 };
