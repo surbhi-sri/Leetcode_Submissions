3bcca5b79e69bbcbaea3c144f510308a2ffd7e3d
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2025-01-18 18:37:53 +0530

    Time: 2 ms (81.22%) | Memory: 12.9 MB (86.70%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
+    void solve(string& s, vector<string>& ans, int n, int open, int close) {
+        if (close > open || open > n || close > n)
+            return;
+        if (s.size() == 2 * n) {
+            if (open ==close)
+                ans.push_back(s);
+            return;
+        }
 
-   bool isValdid(string s){
-    int cnt=0;
-    for (char c : s){
-    if(c=='(') cnt++;
-    else if(c==')') cnt--;
-    if(cnt<0) return 0;
+        s.push_back('(');
+        solve(s, ans, n, open + 1, close);
+        s.pop_back();
+        s.push_back(')');
+        solve(s, ans, n, open, close + 1);
+        s.pop_back();
     }
-    return cnt==0;
-   }
-
-   void solve(string &s, vector<string> &ans, int n){
-    if(s.size()==n){
-        if(isValdid(s)) ans.push_back(s);
-        return;
-    }
-    
-    s.push_back('(');
-    solve(s, ans, n);
-    s.pop_back();
-     s.push_back(')');
-    solve(s, ans, n);
-    s.pop_back();
-
-   }
 
     vector<string> generateParenthesis(int n) {
-    vector<string> ans;
-     string s;
-     solve(s, ans, 2*n);
-     return ans;   
+        vector<string> ans;
+        string s;
+        solve(s, ans, n, 0, 0);
+        return ans;
     }
 };
