782a2dc31a0bf17455c26a2acab0fad03c2c8926
surbhi-sri <105586968+surbhi-sri@users.noreply.github.com>
2025-01-20 11:49:26 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (23.53%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -4,10 +4,9 @@ public:
         if (start == goal)
             return 0;
         int cnt = 0;
-        while (start != 0 || goal != 0) {
-            if ((start & 1) != (goal & 1))
-                cnt++;
-            start = start >> 1;
+        goal ^= start;
+        while (goal != 0) {
+            cnt += (goal & 1);
             goal = goal >> 1;
         }
         return cnt;
