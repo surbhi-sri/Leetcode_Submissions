9b1cd49b3e887443e26f01a5ab3a62c718ad5a62
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-02-27 12:43:58 +0530

    Time: 305 ms (50.85%) | Memory: 15.7 MB (74.57%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int solve(int j, int k, vector<int>& arr, unordered_map<int, int>& mp) {
-        int target = arr[k] - arr[j];
-        if (mp.count(target) && mp[target] < j) {
+        int target = arr[k] + arr[j];
+        if (mp.count(target) && mp[target] >k) {
             int i = mp[target];
 
-            return solve(i, j, arr, mp) + 1;
+            return solve(k, i, arr, mp) + 1;
         }
         return 2;
     }
@@ -20,7 +20,7 @@ public:
 
         int len = 0;
 
-        for (int j = 1; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             for (int k = j + 1; k < n; k++) {
                 int length = solve(j, k, arr, mp);
 
