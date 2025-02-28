9d7210b8bf6c0aaf64f8f98ad06cd413f9d5910d
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-02-28 16:01:50 +0530

    Time: 4 ms (19.81%) | Memory: 14.8 MB (75.16%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -4,25 +4,29 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+        vector<int> dp(matrix[0]);
         int mn = INT_MAX;
 
         for (int i = 0; i < n; i++) {
+            vector<int> temp(m, 0);
             for (int j = 0; j < m; j++) {
-
-                if (i == 0)
-                    dp[i][j] = matrix[i][j];
+                if (i == 0){
+                    dp[j] = matrix[i][j];
+                    temp[j] = matrix[i][j];
+                    }
                 else {
-                    int d = dp[i - 1][j];
-                    int dgr = (j + 1 < m) ? dp[i - 1][j + 1] : INT_MAX;
-                    int dgl = (j > 0) ? dp[i - 1][j - 1] : INT_MAX;
+                    int d = dp[j];
+                    int dgr = (j + 1 < m) ? dp[j + 1] : INT_MAX;
+                    int dgl = (j > 0) ? dp[j - 1] : INT_MAX;
 
-                    dp[i][j] = matrix[i][j] + min({d, dgr, dgl});
+                    temp[j] = matrix[i][j] + min({d, dgr, dgl});
                 }
                 if (i == n - 1)
-                    mn = min(mn, dp[i][j]);
+                    mn = min(mn, temp[j]);
             }
+            dp = temp;
         }
-        return mn;
+
+        return *min_element(dp.begin(), dp.end());
     }
 };
