ea69561c97948d2c521ad13d7ebcc1276ce28bc8
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-02-28 11:15:07 +0530

    Time: 27 ms (38.39%) | Memory: 26.8 MB (43.64%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,53 +1,62 @@
 class Solution {
 public:
-    void lca(string& x, string& y, int n, int m, vector<vector<int>>& dp) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (x[i - 1] == y[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+    string shortestCommonSupersequence(string str1, string str2) {
+
+        int n = str1.size();
+        int m = str2.size();
+
+        if (n == 0)
+            return str2;
+        if (m == 0)
+            return str1;
+
+        int i, j;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (i = 1; i <= n; i++) {
+            for (j = 1; j <= m; j++) {
+
+                if (str1[i - 1] == str2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+
                 else
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-    }
 
-    string shortestCommonSupersequence(string x, string y) {
-        int n = x.size();
-        int m = y.size();
-        if (n == 0)
-            return y;
-        if (m == 0)
-            return x;
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        lca(x, y, n, m, dp);
-        string s = "";
-        int i = n, j = m;
+        string ans = "";
+
         while (i > 0 && j > 0) {
-            if (x[i - 1] == y[j - 1]) {
-                s += x[i - 1];
+
+            if (str1[i - 1] == str2[j - 1]) {
+                ans += str1[i - 1];
                 i--;
                 j--;
             } else {
                 if (dp[i - 1][j] > dp[i][j - 1]) {
-                    s += x[i - 1];
+                    ans += str1[i - 1];
                     i--;
-                } else {
-                    s += y[j - 1];
+                }
+
+                else {
+                    ans += str2[j - 1];
                     j--;
                 }
             }
         }
 
         while (i > 0) {
-            s += x[i - 1];
+            ans += str1[i - 1];
             i--;
         }
+
         while (j > 0) {
-            s += y[j - 1];
+            ans += str2[j - 1];
             j--;
         }
-        reverse(s.begin(), s.end());
-        
-        return s;
+
+        reverse(begin(ans), end(ans));
+        ans.pop_back();
+        return ans;
     }
 };
