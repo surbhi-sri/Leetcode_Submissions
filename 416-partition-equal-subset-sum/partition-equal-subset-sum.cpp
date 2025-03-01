bc7ad77130d1a893c68687d536f84319d8544c3c
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-03-01 19:59:05 +0530

    Time: 139 ms (36.04%) | Memory: 17 MB (68.97%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -6,8 +6,8 @@ public:
         for (int i = 0; i < n; i++)
             dp[i][0] = 1;
 
-        // if (nums[0] <= target)
-        //     dp[0][nums[0]] = 1;
+        if (nums[0] <= target)
+            dp[0][nums[0]] = 1;
 
         for (int i = 1; i < n; i++) {
             for (int j = 0; j <= target; j++) {
