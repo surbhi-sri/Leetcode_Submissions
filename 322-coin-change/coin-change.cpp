68b8f5250873b2988595aa70275c65ecee70f163
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-03-03 15:03:15 +0530

    Time: 46 ms (34.80%) | Memory: 49.5 MB (25.11%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int sum) {
-        int n = coins.size();
+    int countMn(int n, vector<int>& coins, int amount,
+                vector<vector<int>>& dp) {
 
-        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
-
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= sum; j++) {
-                if (j == 0)
-                    dp[i][j] = 0;
-                else if (i == 0)
-                    dp[i][j] = INT_MAX - 1;
-                else if (j < coins[i - 1])
-                    dp[i][j] = dp[i - 1][j];
-                else
-                    dp[i][j] = min(1 + dp[i][j - coins[i - 1]], dp[i - 1][j]);
-            }
+        if (n == 0) {
+            if (amount % coins[0] == 0)
+                return amount / coins[0];
+            else
+                return 1e9;
         }
-        return dp[n][sum] == INT_MAX - 1 ? -1 : dp[n][sum];
+
+        if (dp[n][amount] != -1)
+            return dp[n][amount];
+
+        int notTake = countMn(n - 1, coins, amount, dp);
+
+        int take = INT_MAX;
+
+        if (coins[n] <= amount)
+
+            take = 1 + countMn(n, coins, amount - coins[n], dp);
+
+        return dp[n][amount] = min(notTake, take);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+
+        int cnt = countMn(n - 1, coins, amount, dp);
+
+        return cnt > 1e4 ? -1 : cnt;
     }
 };
