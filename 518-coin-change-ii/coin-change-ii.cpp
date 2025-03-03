d4383644eaa2b25f4abe057a4174b4dedec4c8bd
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-03-03 17:23:45 +0530

    Time: 32 ms (25.58%) | Memory: 46 MB (16.71%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-    int change(int sum, vector<int>& coins) {
+     const int mod = INT_MAX;
+
+    int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<long long int>> dp(n + 1,
-                                         vector<long long int>(sum + 1, 0));
-        for (long long int i = 0; i <= n; i++)
-            dp[i][0] = 1;
-        for (long long int i = 1; i <= n; i++) {
-            for (long long int j = 0; j <= sum; j++) {
-                if (j >= coins[i - 1])
-                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
-                else
-                    dp[i][j] = dp[i - 1][j];
+
+        vector<vector<long>> dp(n, vector<long>(amount + 1, 0));
+
+        for (int j = 0; j <= amount; j++) {
+            if (j % coins[0] == 0)
+                dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= amount; j++) {
+
+                long  notTake = dp[i - 1][j];
+
+                long  take = 0;
+
+                if (j >= coins[i])
+                    take = dp[i][j - coins[i]];
+
+                dp[i][j] = (take + notTake) % mod;
             }
         }
-        return dp[n][sum];
+
+        return dp[n - 1][amount];
     }
 };
