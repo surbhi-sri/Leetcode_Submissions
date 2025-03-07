c9351740f9969cafe45b1f6ae6cd7acd6935c0ca
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-03-07 10:16:49 +0530

    Time: 15 ms (33.65%) | Memory: 16.6 MB (70.73%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int lcs(int n, int m, string& x, string& y) {
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (x[i - 1] == y[j - 1])
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    int minDistance(string s, string t) {
+         int n=s.size();
+         int m=t.size();    
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1]==t[j-1])
+                 dp[i][j] =1+dp[i-1][j-1];
+                else 
+                 dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
             }
         }
-        return dp[n][m];
-    }
-
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        int nlcs = lcs(n, m, word1, word2);
-        return (n + m - 2 * nlcs);
+        
+        return (n+m-dp[n][m]-dp[n][m]);
     }
 };
