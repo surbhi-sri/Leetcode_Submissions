9f6660c27e57aa5b7cf64ca504ecd488a8e1687f
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-03-07 15:32:52 +0530

    Time: 179 ms (61.11%) | Memory: 23.4 MB (36.85%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,51 +1,43 @@
 class Solution {
 public:
-    bool isPrime(int x) {
+    vector<bool> Sieve_of_Eratosthenes(int right) {
+        vector<bool> prime(right + 1, 1);
+        prime[0] = 0;
+        prime[1] = 0;
 
-        if (x <= 1)
-            return 0;
-
-        if (x == 2 || x == 3)
-            return 1;
-
-        if (x % 2 == 0 || x % 3 == 0)
-            return 0;
-
-        for (int i = 5; i * i <= x; i += 6) {
-            if (x % i == 0 || x % (i + 2) == 0)
-                return 0;
-        }
-        return 1;
-    }
-
-    vector<int> closestPrimes(int left, int right) {
-        vector<int> v;
-
-    
-        int k = 0;
-
-        int s, mn = 1000000;
-
-        for (int i = left; i <= right; i++) {
-
-            if (isPrime(i)) {
-                v.push_back(i);
-                k++;
-
-                if (v.size() >= 2) {
-                    if (mn > v[k - 1] - v[k-2]) {
-                        mn = v[k - 1] - v[k-2];
-                        s = k-2;
-                    }
+        for (int i = 2; i*i <= right; i++) {
+            if (prime[i] == 1) {
+                for (int j = i * i; j <= right; j += i) {
+                    prime[j] = 0;
                 }
             }
         }
+        return prime;
+    }
 
-        //  for(int i=0; i<k; i++) cout<<v[i]<<" "<<v[i+1]<<endl;
-    cout<<endl;
-        if (mn == 1000000)
+    vector<int> closestPrimes(int left, int right) {
+
+        if (right <= 2)
             return {-1, -1};
-        else
-            return {v[s], v[s + 1]};
+        vector<bool> isPrime = Sieve_of_Eratosthenes(right);
+
+        vector<int> prime;
+
+        for (int i = left; i <= right; i++) {
+            if (isPrime[i]==1)
+                prime.push_back(i);
+        }
+
+        int mn = 100000000;
+        vector<int> res={-1, -1};
+
+        for (int i = 1; i < prime.size(); i++) {
+            if (prime[i] - prime[i-1] < mn) {
+                mn = prime[i] - prime[i-1];
+                res = {prime[i-1] , prime[i]};
+            }
+        }
+
+        return res;
     }
 };
