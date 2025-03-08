e829a284d911d8c9b3ec28f9225c36642582b187
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-03-08 15:18:49 +0530

    Time: 6 ms (66.74%) | Memory: 10.4 MB (95.23%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -5,20 +5,23 @@ public:
         int n = word1.size();
         int m = word2.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
 
-        for(int i=0; i<=n; i++) dp[i][0]=i;
-        for(int i=0; i<=m; i++) dp[0][i]=i;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (word1[i - 1] == word2[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1];
-                else
-                dp[i][j]= 1+ min({dp[i-1][j-1], dp[i][j-1], dp[i-1][j]});
-            }
+        for(int i=0; i<=m; i++) {
+            prev[i]=i;
         }
 
-        return dp[n][m];
+        for (int i = 1; i <= n; i++) {
+            curr[0]=i;
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1])
+                    curr[j] = prev[j - 1];
+                else
+                curr[j]= 1+ min({prev[j-1], curr[j-1], prev[j]});
+            }
+            prev=curr;
+        }
+
+        return prev[m];
     }
 };
