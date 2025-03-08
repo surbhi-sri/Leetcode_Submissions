a8801a42f12eba8db248d7a975ea3da48833ee50
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-03-08 12:04:34 +0530

    Time: 16 ms (83.26%) | Memory: 8.8 MB (94.66%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -6,20 +6,20 @@ public:
         int n = s.size();
         int m = t.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        vector<int> prev(m + 1, 0), curr(m + 1, 0);
 
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = 1;
+        prev[0] = curr[0] = 1;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (s[i - 1] == t[j - 1])
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+                    curr[j] = (prev[j - 1] + prev[j]) % mod;
                 else
-                    dp[i][j] = dp[i - 1][j];
+                    curr[j] = prev[j];
             }
+            prev=curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
