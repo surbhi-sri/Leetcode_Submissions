2b650fc30f9862adeb0ed79598ae47cc7fd8fda7
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-03-09 17:26:29 +0530

    Time: 4 ms (21.42%) | Memory: 16 MB (64.48%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -8,16 +8,10 @@ public:
         vector<vector<int>> dp(n + 2, vector<int>(2, 0));
 
         for (int ind = n - 1; ind >= 0; ind--) {
-            for (int buy = 0; buy < 2; buy++) {
-                int profit = 0;
 
-                if (buy)
-                    profit = max(-prices[ind] + dp[ind + 1][0], dp[ind + 1][1]);
-                else
-                    profit =max(prices[ind] + dp[ind + 2][1], dp[ind + 1][0]);
+            dp[ind][1] = max(-prices[ind] + dp[ind + 1][0], dp[ind + 1][1]);
 
-                dp[ind][buy] = profit;
-            }
+            dp[ind][0] = max(prices[ind] + dp[ind + 2][1], dp[ind + 1][0]);
         }
 
         return dp[0][1];
