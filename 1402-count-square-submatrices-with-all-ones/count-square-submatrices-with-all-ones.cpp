c42eda58b4200e89db21a38dbc7646184547d495
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-03-14 00:09:03 +0530

    Time: 7 ms (53.16%) | Memory: 30.3 MB (42.50%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    bool isSquare(vector<vector<int>>& matrix, int r, int c, int size) {
-
-        for (int i = r; i < r + size; i++) {
-            for (int j = c; j < c + size; j++) {
-                if (matrix[i][j] == 0)
-                    return 0;
-            }
-        }
-
-        return 1;
-    }
-
     int countSquares(vector<vector<int>>& matrix) {
-
-        int cnt = 0;
+        vector<vector<int>> dp = matrix;
 
         int n = matrix.size();
         int m = matrix[0].size();
 
+        int cnt = 0;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
 
-                for (int size = 1; (size + i <= n && size + j <= m); size++) {
-                    if (isSquare(matrix, i, j, size))
-                        cnt++;
-                }
+                if (matrix[i][j] == 0|| i==0 || j==0)
+                    dp[i][j] = matrix[i][j];
+                else
+                    dp[i][j] =
+                        1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
+
+                 cnt+=dp[i][j];       
             }
         }
 
