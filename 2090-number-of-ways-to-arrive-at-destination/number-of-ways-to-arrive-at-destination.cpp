459771bd7004eb3ebf1d8b1edffa53ac0f3db0ae
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-03-23 17:37:51 +0530

    Time: 12 ms (47.05%) | Memory: 35.1 MB (69.85%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int, int>>> adj(n);
+        vector<vector<pair<int, int>>> adj(n+1);
 
         for (auto &road : roads) {
             adj[road[0]].push_back({road[1], road[2]});
@@ -13,8 +13,8 @@ public:
             pq;
 
         pq.push({0, 0});
-        vector<long long> time(n, LLONG_MAX);
-        vector<int> ways(n, 0);
+        vector<long long> time(n+1, LLONG_MAX);
+        vector<int> ways(n+1, 0);
 
         time[0] = 0;
         ways[0]=1;
