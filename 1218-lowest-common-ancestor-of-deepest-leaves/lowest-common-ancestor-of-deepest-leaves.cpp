1857b688ad56f10e73e1a755170e9299866c08a0
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-04-04 23:47:00 +0530

    Time: 4 ms (13.33%) | Memory: 22 MB (67.37%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -12,35 +12,28 @@
  */
 class Solution {
 public:
-    unordered_map<int, int> mp;
-    int maxD = 0;
+    pair<int, TreeNode*> LCA(TreeNode* root) {
+        if (root == NULL)
+            return {0, root};
 
-    TreeNode* LCA(TreeNode* root) {
-        if (root == NULL || mp[root->val] == maxD)
-            return root;
+        if (root->left == NULL && root->right == NULL)
+            return {1, root};
 
-        TreeNode* l = LCA(root->left);
-        TreeNode* r = LCA(root->right);
+        pair<int, TreeNode*> l = LCA(root->left);
+        pair<int, TreeNode*> r = LCA(root->right);
 
-        if(l==NULL) return r;
-        if(r==NULL) return l;
-        
-        return root;
-    }
+        if (l.first > r.first)
+            return {l.first + 1, l.second};
 
-    void depth(TreeNode* root, int dis) {
-        if (!root)
-            return;
+        else if (l.first < r.first)
+            return {r.first + 1, r.second};
 
-        maxD = max(maxD, dis);
-        mp[root->val] = dis;
-        depth(root->left, dis + 1);
-        depth(root->right, dis + 1);
-        return;
+        return {l.first+1, root};
     }
 
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        depth(root, 0);
-        return LCA(root);
+        pair<int, TreeNode*> lca = LCA(root);
+
+        return lca.second;
     }
 };
