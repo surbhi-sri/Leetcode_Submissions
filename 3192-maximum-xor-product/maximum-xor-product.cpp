e847dd24e3778288d22ff706794bdab6be383c68
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-04-21 18:54:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (87.44%) - LeetSync

--- 3192-maximum-xor-product/maximum-xor-product.cpp
+++ 3192-maximum-xor-product/maximum-xor-product.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int m = 1e9 + 7;
     typedef long long ll;
-
     int maximumXorProduct(long long a, long long b, int n) {
         ll xorra = 0;
         ll xorrb = 0;
 
-        for (ll i = 49; i >= n; i--) {
+        for (int i = 49; i >= n; i--) {
             bool a_ith_bit = ((a >> i) & 1) > 0;
             bool b_ith_bit = ((b >> i) & 1) > 0;
 
-            if (a_ith_bit == 1)
+            if (a_ith_bit)
                 xorra = xorra ^ (1ll << i);
-
-            if (b_ith_bit == 1)
+            if (b_ith_bit)
                 xorrb = xorrb ^ (1ll << i);
         }
 
-        for (ll i = n - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             bool a_ith_bit = ((a >> i) & 1) > 0;
             bool b_ith_bit = ((b >> i) & 1) > 0;
 
@@ -36,6 +34,6 @@ public:
         xorra = xorra % m;
         xorrb = xorrb % m;
 
-        return (xorra*xorrb)%m;
+        return (xorra * xorrb) % m;
     }
 };
