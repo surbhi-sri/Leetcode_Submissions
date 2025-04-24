4afcf4144b644cc00b141d36eef4c3b57505453e
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-04-24 22:07:02 +0530

    Time: 330 ms (15.82%) | Memory: 158.4 MB (15.55%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
+    unordered_map<string, int> mp;
 
-    bool hasduplicate(string &a, string &b){
+    bool hasduplicate(string& a, string& b) {
         unordered_set<char> st;
-        for(char ch: a){
-            if(st.count(ch)>0) return 1;
+        for (char ch : a) {
+            if (st.count(ch) > 0)
+                return 1;
             st.insert(ch);
         }
 
-        for(char ch: b){
-            if(st.count(ch)>0) return 1;
+        for (char ch : b) {
+            if (st.count(ch) > 0)
+                return 1;
             st.insert(ch);
         }
 
@@ -20,6 +23,9 @@ public:
         if (i >= n)
             return temp.size();
 
+        if (mp.find(temp) != mp.end())
+            return mp[temp];
+
         int include = 0;
         int exclude = 0;
 
@@ -29,12 +35,13 @@ public:
 
         exclude = solve(i + 1, temp, arr, n);
 
-        return max(include, exclude);
+        return mp[temp] = max(include, exclude);
     }
 
     int maxLength(vector<string>& arr) {
         string temp = "";
         int n = arr.size();
+        mp.clear();
 
         return solve(0, temp, arr, n);
     }
