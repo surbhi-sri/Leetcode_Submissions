9631fcd4d1d0987230a4baf8cecb2c0998436b38
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-04-27 17:14:27 +0530

    Time: 8 ms (23.67%) | Memory: 264 MB (26.76%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-     int n=derived.size();
-
-     int takingBit=1;
-     int temp=takingBit;
+        bool xorr=0;
      
-     for(int i=0; i<n-1; i++){
-        if(derived[i+1]==1) temp=!temp;
-     }
+          for(int d: derived)
+          xorr ^=d;
 
-     if(derived[0]==1) temp=!temp;
+          return xorr==0;
 
-     return takingBit==temp;
     }
 };
