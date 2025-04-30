aa4b8b3455be7ce82c05b9c52fe4f2f29205acac
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-04-30 20:04:13 +0530

    Time: 153 ms (47.16%) | Memory: 50.7 MB (86.56%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,69 +1,36 @@
 class Solution {
 public:
-    struct trie {
-        bool isend;
-        trie* child[26];
-    };
-
-    trie*
-    getnode() {
-        trie* node = new trie();
-        node->isend = 0;
-        for (int i = 0; i < 26; i++)
-            node->child[i] = NULL;
-
-        return node;
-    }
-
-     trie* root;
-
-    void insert(string &word) {
-        trie* node = root;
-        int n = word.size();
-
-        for (int i = 0; i < n; i++) {
-            int ch_idx = word[i] - 'a';
-            if (node->child[ch_idx] == NULL)
-                node->child[ch_idx] = getnode();
-
-            node = node->child[ch_idx];
-        }
-        node->isend=1;
-    }
-
-    string search(string &word){
-         trie* node = root;
-        int n = word.size();
-
-        for (int i = 0; i < n; i++){
-             int ch_idx = word[i] - 'a';
-             if (node->child[ch_idx] == NULL)
-             return word;
-             else 
-             node = node->child[ch_idx];
-             if(node->isend==1) return word.substr(0, i+1);
-        }
-
-        return word;
-    }
-
     string replaceWords(vector<string>& dictionary, string sentence) {
-       root=getnode();
+        unordered_set<string> st;
+
         int n = dictionary.size();
 
         for (int i = 0; i < n; i++)
-            insert(dictionary[i]);
+            st.insert(dictionary[i]);
 
         stringstream ss(sentence);
         string word;
-
         string res;
 
-        while(getline(ss, word, ' '))
-        res+=search(word)+" ";
+        while (getline(ss, word, ' ')) {
+            string str;
+            int found = 0;
+            for (char ch : word) {
+                str += ch;
+                if (st.find(str) != st.end()) {
+                    found = 1;
+                    res += str + " ";
+                    break;
+                }
+            }
+            if (found)
+                continue;
+            else
+                res += word + " ";
+        }
 
         res.pop_back();
-        
+
         return res;
     }
 };
