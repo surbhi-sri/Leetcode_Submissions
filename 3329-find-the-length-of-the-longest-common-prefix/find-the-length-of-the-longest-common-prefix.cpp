d9b70c694012d6d408be4c02696e69db615dca32
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-04-30 23:30:33 +0530

    Time: 140 ms (69.65%) | Memory: 160.7 MB (58.39%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,27 +1,62 @@
 class Solution {
 public:
-     
+    struct trie {
+      //  char digit;
+        trie* child[10];
+    };
+
+    trie* getnode() {
+        trie* node = new trie();
+        for (int i = 0; i < 10; i++)
+            node->child[i] = NULL;
+
+        return node;
+    }
+trie* root;
+    void insert(int num) {
+        trie* crawler = root;
+
+        string st = to_string(num);
+        int n = st.size();
+
+        for (int i = 0; i < n; i++) {
+            int idx = st[i] - '0';
+            if (crawler->child[idx] == NULL)
+                crawler->child[idx] = getnode();
+
+            crawler = crawler->child[idx];
+        }
+    }
+
+    int search(int num) {
+        trie* crawler = root;
+
+        string st = to_string(num);
+        int n = st.size();
+        int i = 0;
+
+        for (i = 0; i < n; i++) {
+            int idx = st[i] - '0';
+            if (crawler->child[idx] == NULL)
+                return i;
+            else
+                crawler = crawler->child[idx];
+        }
+        return i;
+    }
 
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int> st;
+         root = getnode();
 
-        for(int num: arr1){
-            while(!st.count(num) && num>0){
-                st.insert(num);
-                num /=10;
-            }
+        for (int num : arr1) {
+            insert(num);
         }
 
-        int res=0;
-         
-        for(int num: arr2){
-            while(!st.count(num) && num>0)
-                num /=10;
-            
-            if(num>0)
-            res= max(res, static_cast<int>(log10(num)+1));
+        int len = 0;
+        for (int num : arr2) {
+            int curr_len = search(num);
+            len = max(len, curr_len);
         }
-
-        return res;
+        return len;
     }
 };
