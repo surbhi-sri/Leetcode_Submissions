5fae25b166aa6b14685cdb9e36ae101190ef0239
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-05-02 20:24:41 +0530

    Time: 23 ms (5.51%) | Memory: 44.5 MB (5.17%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -1,23 +1,84 @@
+struct trie {
+    trie* child[26];
+    bool isend = 0;
+};
+
+trie* getnode() {
+    trie* node = new trie();
+    node->isend = 0;
+
+    for (int i = 0; i < 26; i++)
+        node->child[i] = NULL;
+
+    return node;
+}
+
+class trienode {
+public:
+    trie* root;
+
+    trienode() { root = getnode(); }
+
+    void insert(string s) {
+        trie* crawler = root;
+
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            int idx = s[i] - 'a';
+            if (crawler->child[idx] == NULL)
+                crawler->child[idx] = getnode();
+
+            crawler = crawler->child[idx];
+        }
+        crawler->isend = 1;
+    }
+
+    bool search(string s) {
+        trie* crawler = root;
+
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            int idx = s[i] - 'a';
+            if (crawler->child[idx] == NULL)
+                return 0;
+
+            crawler = crawler->child[idx];
+        }
+        return 1;
+    }
+};
+
 class Solution {
 public:
     int countPrefixSuffixPairs(vector<string>& words) {
-        int n = words.size();
+
         int cnt = 0;
+        int n = words.size();
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
+        for (int j = 1; j < n; j++) {
 
-                if (words[j].size() >= words[i].size()) {
-                    string s1 = words[i];
-                    string s2 = words[j];
-                    int len = s1.size();
+            trienode root1;
+            trienode root2;
 
-                    if (s1 == s2.substr(0, len) &&
-                        s1 == s2.substr(s2.length() - len))
-                        cnt++;
-                }
+            string temp = words[j];
+            reverse(temp.begin(), temp.end());
+
+            root1.insert(words[j]);
+            root2.insert(temp);
+
+            for (int i = 0; i < j; i++) {
+
+                if (words[i] > words[j])
+                    continue;
+
+                string rev = words[i];
+                reverse(rev.begin(), rev.end());
+
+                if (root1.search(words[i]) == 1 && root2.search(rev) == 1)
+                    cnt++;
             }
         }
+
         return cnt;
     }
 };
