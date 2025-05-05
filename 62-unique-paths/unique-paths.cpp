20075d251ea4fb21399f8aa587a60d9d7cc5456f
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-05-05 18:15:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (56.17%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        //here we will use combination
-        //total number of direction :n
-        //like in 2x3 matrix= there will be two right as r and one down as d;
-        //so total number of combination will :nCr  or nCd
-        // ans: rrd, rdr, drr;
+    int solve(int i, int j, int n, int m) {
+        if (i == n - 1 && j == m - 1)
+            return 1;
 
-        int N=m-1+n-1;
-        int direction =min(m-1,n-1);
+        if (i > n || j > m)
+            return 0;
 
-        long long res=1;
+        return solve(i + 1, j, n, m) + solve(i, j + 1, n, m);
+    }
 
-        for(int i=1; i<=direction; i++){
-            res = res*(N-i+1)/i;
+    int uniquePaths(int n, int m) {
+        vector<vector<int>>dp(n, vector<int>(m, 0));
+        dp[n-1][m-1]=1;
+        
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+              if(i+1<n) dp[i][j] +=dp[i+1][j];
+              if(j+1<m) dp[i][j] +=dp[i][j+1];
+            }
         }
 
-        return res;
-    }
+        return dp[0][0];
+        
+        }
 };
