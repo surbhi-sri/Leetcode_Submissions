bf260e61d70b4065e98d9081582bf255a84f74d1
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-05-27 18:58:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (70.35%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int sum=0;
+        
+        int total_sum= (n*(n+1))/2;  // from 1 to n
 
-        for(int i=1; i<=n; i++){
-            if(i%m!=0) sum+=i;
-            else sum -=i;
-        }
+        int k= n/m;  //total Number Divisible By M
 
-        return sum;
+        int sum2= (m*k*(k+1))/2;   //sum Divisible By M
+
+        return total_sum-2*sum2;  
     }
 };
