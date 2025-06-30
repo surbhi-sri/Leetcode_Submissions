57138552644fcaa8e60db5f1f5c5938bf5c2297e
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-06-30 11:53:27 +0530

    Time: 11 ms (86.10%) | Memory: 36.2 MB (84.89%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -2,31 +2,35 @@ class Solution {
 public:
     int findLHS(vector<int>& nums) {
         int n = nums.size();
-        map<int, int> mp;
-
-        for (int i = 0; i < n; i++)
-            mp[nums[i]]++;
-
-        queue<pair<int, int>> q;
-
-        for (auto& m : mp)
-            q.push({m.first, m.second});
-
         int len = 0;
-        int ele1 = q.front().first;
-        int cnt1 = q.front().second;
-        q.pop();
 
-        while (!q.empty()) {
-            int ele2 = q.front().first;
-            int cnt2 = q.front().second;
-            q.pop();
-            if (ele1 + 1 == ele2) {
+        sort(nums.begin(), nums.end());
+
+        int ele1 = nums[0];
+        int cnt1 = 0;
+        int j = 0;
+
+        while (j < n && nums[j] == ele1) {
+            j++;
+            cnt1++;
+        }
+
+        while (j < n) {
+            int ele2 = nums[j];
+            int cnt2 = 0;
+
+            while (j < n && nums[j] == ele2) {
+                j++;
+                cnt2++;
+            }
+
+            if (ele1 == ele2 - 1) {
                 len = max(len, cnt1 + cnt2);
             }
             ele1 = ele2;
             cnt1 = cnt2;
         }
+
         return len;
     }
 };
