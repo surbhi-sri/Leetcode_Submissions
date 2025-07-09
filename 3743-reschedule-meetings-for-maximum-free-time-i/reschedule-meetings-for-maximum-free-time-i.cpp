e86df230241cbeafb0f01147a5d9c4fb6b9d5900
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-07-09 17:59:49 +0530

    Time: 8 ms (33.10%) | Memory: 125.8 MB (51.19%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
@@ -4,42 +4,33 @@ public:
                     vector<int>& endTime) {
         int n = startTime.size();
         vector<int> freeTime;
-        int e = 0;
 
-        for(int i = 0; i <= n; i++) {
-            if(i==0 && e==startTime[i]){
-                e=endTime[i];
-                continue;
-            }
-            if (i == n) {
-                    freeTime.push_back(eventTime - e);
- //                     cout<<freeTime.back()<<" ";
-                break;
-            }   
-              freeTime.push_back(startTime[i] - e);
-              e = endTime[i];
-//              cout<<freeTime.back()<<" ";
+        freeTime.push_back(startTime[0] - 0);
+
+        for (int i = 1; i < n; i++)
+            freeTime.push_back(startTime[i] - endTime[i - 1]);
+
+        freeTime.push_back(eventTime - endTime[n - 1]);
+
+        int time = 0;
+        n = freeTime.size();
+
+        for (int i = 0; i < min(n, k + 1); i++) {
+            time += freeTime[i];
         }
 
-         int  time=0;
-         n=freeTime.size();
-        
-         for(int i=0; i<min(n , k+1); i++){
-            time+=freeTime[i];
-         }
+        if (n <= k + 1)
+            return time;
 
-        //  cout<<time<<" ";
+        int maxT = time;
+        int i = 0;
 
-         if(n<= k+1) return time;
-         int maxT=time;
-         int i=0;
-
-         for(int j=k+1; j<n; j++, i++){
-           time +=(freeTime[j]-freeTime[i]);
-           maxT=max(maxT, time);
+        for (int j = k + 1; j < n; j++, i++) {
+            time += (freeTime[j] - freeTime[i]);
+            maxT = max(maxT, time);
             // cout<<time<<" ";
-         }
+        }
 
-         return maxT;
+        return maxT;
     }
 };
