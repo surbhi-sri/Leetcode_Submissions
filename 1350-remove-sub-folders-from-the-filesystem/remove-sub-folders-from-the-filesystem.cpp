35a6618c6e0613d8dd17be19afb7de3d42c8d7fd
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-07-20 00:14:51 +0530

    Time: 148 ms (26.88%) | Memory: 69.1 MB (41.29%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-      bool prefixCheck(string& str, string& folder) {
-        int n = str.length(), m = folder.length();
-        if (m <= n) return false;
-
-        int i = 0;
-        while (i < n) {
-            if (str[i] != folder[i])
-                return false;
-            i++;
-        }
-
-        return folder[i] == '/';
-    }
-
     vector<string> removeSubfolders(vector<string>& folder) {
         sort(folder.begin(), folder.end());
+        unordered_set<string> st;
         vector<string> ans;
 
-        int i = 0, n = folder.size();
-        while (i < n) {
-            string str = folder[i];
-            ans.push_back(str);
-            i++;
-            while (i < n && prefixCheck(str, folder[i])) {
-                i++; // skip subfolders
+        for(string &f:folder){
+          string str="";
+          int flag=1;
+          for(int i=0; i<f.size(); i++){
+            str =str+f[i];
+            if(i<f.size() && f[i+1]=='/' && st.find(str)!=st.end()){
+                flag=0;
+                break;
             }
+          }
+           if(flag){
+            ans.push_back(f);
+            st.insert(f);
+           }
         }
+
         return ans;
-        
     }
 };
