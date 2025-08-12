5e701eaf52ca43cb0ce3ee451631af5b6ec035ce
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-08-12 23:52:00 +0530

    Time: 305 ms (37.69%) | Memory: 275.9 MB (10.24%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -2,32 +2,29 @@ int mod = 1e9 + 7;
 
 class Solution {
 public:
-    int solve(long long sum, int ind, int n, int x, vector<vector<int>>& dp) {
-        if (sum > n)
-            return 0;
-
-        if (sum == n)
+    int solve(long long n, int ind, int x, vector<vector<int>>& dp) {
+        if (n == 0)
             return 1;
 
-        if (ind > n)
+        if (n < 0)
             return 0;
 
-        if (dp[sum][ind] != -1)
-            return dp[sum][ind];
+        if (dp[n][ind] != -1)
+            return dp[n][ind];
 
         long long temp = pow(ind, x);
 
         if (temp > n)
-            return dp[sum][ind] = 0;
+            return dp[n][ind] = 0;
 
-        int take = (solve(sum + temp, ind + 1, n, x, dp)) % mod;
-        int skip = (solve(sum, ind + 1, n, x, dp)) % mod;
+        int take = (solve(n - temp, ind + 1, x, dp)) % mod;
+        int skip = (solve(n, ind + 1, x, dp)) % mod;
 
-        return dp[sum][ind] = (take + skip) % mod;
+        return dp[n][ind] = (take + skip) % mod;
     }
 
     int numberOfWays(int n, int x) {
         vector<vector<int>> dp(n + 1, vector<int>(n + 2, -1));
-        return solve(0, 1, n, x, dp);
+        return solve(n, 1, x, dp);
     }
 };
