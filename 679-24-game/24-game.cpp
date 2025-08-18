e891091df6aa9041b250c3522e839d7c13cd68af
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-08-19 00:08:24 +0530

    Time: 63 ms (20.48%) | Memory: 30.2 MB (21.36%) - LeetSync

---
+++ 679-24-game/24-game.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    double epsilon = 0.1;
+
+    bool solve(vector<double>& cards) {
+        if (cards.size() == 1)
+            return abs(cards[0] - 24.0) <= epsilon;
+
+        for (int i = 0; i < cards.size(); i++) {
+            for (int j = 0; j < cards.size(); j++) {
+                if (i == j)
+                    continue;
+
+                vector<double> temp;
+
+                for (int k = 0; k < cards.size(); k++)
+                    if (k != i && k != j)
+                        temp.push_back(cards[k]);
+
+                double a = cards[i];
+                double b = cards[j];
+
+                vector<double> possibleVal = {a + b, a - b, b - a, a * b};
+
+                if (b > 0)
+                    possibleVal.push_back(a / b);
+                if (a > 0)
+                    possibleVal.push_back(b / a);
+
+                for (double& val : possibleVal) {
+                    temp.push_back(val);
+
+                    if (solve(temp))
+                        return 1;
+                    temp.pop_back();
+                }
+            }
+        }
+        return 0;
+    }
+
+    bool judgePoint24(vector<int>& cards) {
+        vector<double> num;
+
+        for (int i = 0; i < 4; i++)
+            num.push_back(1.0 * cards[i]);
+
+        return solve(num);
+    }
+};
