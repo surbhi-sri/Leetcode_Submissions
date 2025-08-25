c98f6e4377a4e0527b67992bec94693a97798779
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-08-25 20:02:49 +0530

    Time: 1 ms (23.74%) | Memory: 9.2 MB (55.92%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int n = 1;
-        int i = 0;
-        while (i < s.size() && s[i] == ' ')
-            i++;
-        if (i < s.size() && s[i] == '-') {
-            i++;
-            n = 0 - n;
-        } else if (i < s.size() && s[i] == '+')
-            i++;
+      int i=0;
+      int n=s.size();
+      int sign=0;
+      long long num=0;
 
-        long long int num = 0;
+      while(s[i]==' ') i++;
 
-        while (i < s.size() && s[i] >= '0' && s[i] <= '9') {
+      if(s[i]=='-' || s[i]=='+') {
+       if(s[i]=='-') sign=1;
+      i++; 
+      }
 
-            num = (num * 10) + (s[i] - '0');
-            if (num > INT_MAX) {
-                if (n > 0)
-                    return INT_MAX;
-                else
-                    return INT_MIN;
-            }
-            i++;
+     while(s[i]=='0') i++;
+
+     while(s[i]>='0' && s[i]<='9' ){
+        num=num*10+ s[i]-'0';
+        
+        if(num>INT_MAX){
+            if(sign) return INT_MIN;
+            else return INT_MAX;
         }
 
-        if (n < 0)
-            return (0 - num);
-        else
-            return num;
+        i++;
+     }
+
+      if(sign) num=0-num;
+
+      return num;
     }
 };
