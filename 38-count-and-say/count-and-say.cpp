214d3237f2cf448d3e8e83cef27c1909e8e98f79
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-08-26 23:33:23 +0530

    Time: 3 ms (82.95%) | Memory: 10.1 MB (36.55%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -3,36 +3,25 @@ public:
     string countAndSay(int n) {
         if (n == 1)
             return "1";
-
-        string str = countAndSay(n - 1);
-
-        int len = str.size();
-        string res;
-
-        int i = 0, j = 1;
-        char ch = str[0];
-
-        while (1) {
-            if (j < len) {
-                if (ch == str[j])
-                    j++;
-                else {
-                    char num = '0' + (j - i);
-                    res.push_back(num);
-                    res.push_back(ch);
-
-                    ch = str[j];
-                    i = j;
-                }
-            } else {
-                char num = '0' + (j - i);
-                res.push_back(num);
-                res.push_back(ch);
-
-                break;
+        string s = countAndSay(n - 1);
+        string ans = "";
+        char c =s[0];
+        int cnt = 1;
+        int len=s.size();
+        for (int i=1; i<len; i++) {
+         
+            if (s[i] == c)
+                cnt++;
+            else {
+                ans += to_string(cnt);
+                ans.push_back(c);
+                c = s[i];
+                cnt = 1;
             }
         }
+        ans += to_string(cnt);
+        ans.push_back(c);
 
-        return res;
+        return ans;
     }
 };
