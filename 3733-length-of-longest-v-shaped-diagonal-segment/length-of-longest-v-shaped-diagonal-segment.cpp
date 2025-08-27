357651050a971fefd9890625d15a3a569bc8dab1
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-08-28 01:01:46 +0530

    Time: 1632 ms (30.48%) | Memory: 95.2 MB (92.51%) - LeetSync

--- 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,66 +1,41 @@
 class Solution {
 public:
-   int dp[501][501][4][2];
-    int di[4] = {1, 1, -1, -1};
-    int dj[4] = {1, -1, -1, 1};
+    vector<vector<int>> dir = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
     int n, m;
-    vector<vector<int>> g;
-    bool isValid(int x, int y){
-        if(x>=0 && x<n && y>=0 && y<m) return true;
-        return false;
+
+    int solve(int i, int j, int d, bool canTurn, int val,
+              vector<vector<int>>& grid) {
+
+        int i_ = i + dir[d][0];
+        int j_ = j + dir[d][1];
+
+        if (i_ < 0 || j_ < 0 || i_ >= n || j_ >= m || grid[i_][j_] != val)
+            return 0;
+
+        int keepMoving = 1 + solve(i_, j_, d, canTurn, val == 2 ? 0 : 2, grid);
+
+        if (canTurn)
+            keepMoving = max(keepMoving, 1 + solve(i_, j_, (d + 1) % 4, 0,
+                                                   val == 2 ? 0 : 2, grid));
+
+        return keepMoving;
     }
-    int rec(int i, int j, int dir, int left)
-    {
-        if(dp[i][j][dir][left]!=-1) return dp[i][j][dir][left];
-        int ans = 0;
-        if(g[i][j]%2==0){
-            ans=1;
-            int ni = i+di[dir];
-            int nj = j+dj[dir];
-            if(isValid(ni, nj))
-            {
-                if(g[ni][nj]+g[i][j]==2)
-                {
-                    ans += rec(ni, nj, dir, left);
-                }
-            }
-            if(left){
-                int k = (dir+1)%4;
-                int ni = i+di[k];
-                int nj = j+dj[k];
-                if(isValid(ni, nj))
-                {
-                    if(g[ni][nj]+g[i][j]==2)
-                    {
-                        ans = max(ans, 1+rec(ni, nj, k, left-1));
-                    }
-                }
-            }
-        }
-        return dp[i][j][dir][left]=ans;
-    }
+
     int lenOfVDiagonal(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-        g=grid;
-        memset(dp, -1, sizeof(dp));
-        int ans = 0;
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<m; j++)
-            {
-                if(grid[i][j]==1)
-                {
-                    ans = max(ans, 1);
-                    for(int k=0; k<4; k++)
-                    {
-                        int ni = i+di[k], nj = j+dj[k];
-                        if(isValid(ni, nj) && grid[ni][nj]==2)
-                            ans = max(ans, 1+rec(ni, nj, k, 1));
-                    }
+
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    for (int d = 0; d < 4; d++)
+                        res = max(res, 1 + solve(i, j, d, 1, 2, grid));
                 }
             }
         }
-        return ans;
+
+        return res;
     }
 };
