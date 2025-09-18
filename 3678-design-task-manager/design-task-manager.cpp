93725abf38373752ea87440f8cb21327a3c8ef96
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-09-18 22:34:18 +0530

    Time: 354 ms (47.81%) | Memory: 370.5 MB (49.69%) - LeetSync

---
+++ 3678-design-task-manager/design-task-manager.cpp
@@ -0,0 +1,58 @@
+class TaskManager {
+public:
+    set<pair<int, int>> taskTop;
+    unordered_map<int, pair<int, int>> taskMp;
+
+    TaskManager(vector<vector<int>>& tasks) {
+        for (vector<int>& task : tasks) {
+            int userId = task[0];
+            int taskId = task[1];
+            int priority = task[2];
+
+            taskTop.insert({-priority, -taskId});
+            taskMp[taskId] = {userId, priority};
+        }
+    }
+
+    void add(int userId, int taskId, int priority) {
+        taskMp[taskId] = {userId, priority};
+        taskTop.insert({-priority, -taskId});
+    }
+
+    void edit(int taskId, int newPriority) {
+        int oldP = taskMp[taskId].second;
+        taskTop.erase({-oldP, -taskId});
+        taskTop.insert({-newPriority, -taskId});
+        taskMp[taskId].second = newPriority;
+    }
+
+    void rmv(int taskId) {
+        int oldP = taskMp[taskId].second;
+        taskTop.erase({-oldP, -taskId});
+        taskMp.erase(taskId);
+    }
+
+    int execTop() {
+        if (taskTop.empty())
+            return -1;
+
+        auto top_ele = *taskTop.begin();
+
+        int taskId = -1 * top_ele.second;
+        int userId = taskMp[taskId].first;
+        
+        taskTop.erase(top_ele);
+        taskMp.erase(taskId);
+
+        return userId;
+    }
+};
+
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager* obj = new TaskManager(tasks);
+ * obj->add(userId,taskId,priority);
+ * obj->edit(taskId,newPriority);
+ * obj->rmv(taskId);
+ * int param_4 = obj->execTop();
+ */
