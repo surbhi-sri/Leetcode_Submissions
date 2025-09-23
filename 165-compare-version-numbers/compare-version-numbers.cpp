f1180a148388e483fb2d5c2476245ef124dd2fc1
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-09-23 22:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (38.53%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     int compareVersion(string version1, string version2) {
-        stringstream ss1(version1);
-        stringstream ss2(version2);
-
         vector<int> vec;
+        stringstream s1(version1);
+        stringstream s2(version2);
+
         string num;
 
-        while (getline(ss1, num, '.')) {
+        while (getline(s1, num, '.')) {
             vec.push_back(stoi(num));
         }
+
         int i = 0, n = vec.size();
 
-        while (getline(ss2, num, '.')) {
+        while (getline(s2, num, '.')) {
             int val = stoi(num);
-            if (i >= n && val > 0)
+            if ((i >= n && val > 0) || (i < n && val > vec[i]))
                 return -1;
-            else if (i<n && vec[i] > val)
+            else if (i < n && val < vec[i])
                 return 1;
-            else if (i<n && vec[i] < val)
-                return -1;
             else
                 i++;
         }
 
-        while(i<n){
-            if(vec[i]>0) return 1;
+        while (i < n) {
+            if (vec[i] > 0)
+                return 1;
             i++;
         }
 
