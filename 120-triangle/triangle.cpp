a31779045028c89c4586106293c414f9a370661f
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2025-09-25 19:15:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (81.37%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -5,17 +5,17 @@ public:
        int n= triangle.size();
        if(n==1) return triangle[0][0];
 
-       vector<vector<int>> dp(n, vector<int>(n, 0));
+      vector<int> dp(n, 0);
 
        for(int i=0; i<n; i++)
-       dp[n-1][i]=triangle[n-1][i];
+       dp[i]=triangle[n-1][i];
 
        for(int r=n-2; r>=0; r--){
-          for(int c=r; c>=0; c--){
-            dp[r][c]=triangle[r][c] + min(dp[r+1][c], dp[r+1][c+1]);
+          for(int c=0; c<=r; c++){
+            dp[c]=triangle[r][c] + min(dp[c], dp[c+1]);
           }
        }
 
-       return dp[0][0];
+       return dp[0];
     }
 };
