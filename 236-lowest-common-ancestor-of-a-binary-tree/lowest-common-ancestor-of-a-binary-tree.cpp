6b0b1ff3fce450b4d87e23832c6579a9667ae7ef
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2026-01-09 23:23:59 +0530

    Time: 7 ms (92.58%) | Memory: 17.2 MB (87.35%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,35 +9,16 @@
  */
 class Solution {
 public:
-    bool path(TreeNode* root, TreeNode* node, vector<TreeNode*>& paths) {
-        if (!root)
-            return 0;
-
-        paths.push_back(root);
-
-        if (root == node || path(root->left, node, paths) ||
-            path(root->right, node, paths))
-            return 1;
-
-        paths.pop_back();
-        return 0;
-    }
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path1, path2;
-        path(root, p, path1);
-        path(root, q, path2);
+     if(!root) return NULL;
+        if(root==p) return p;
+        else if(root==q) return q;
+        
+        TreeNode *l=lowestCommonAncestor(root->left,p,q);
+        TreeNode *r=lowestCommonAncestor(root->right,p,q);
 
-        int n = min(path1.size(), path2.size());
-
-        TreeNode* lca = path1[n - 1];
-        for (int i = 0; i < n; i++) {
-            if (path1[i] != path2[i]) {
-                lca = path1[i - 1];
-                break;
-            }
-        }
-
-        return lca;
+        if(l && r) return root;
+        else if(l) return l;
+        else return r;
     }
 };
