bbe94654c3d2ccf8783962aad52dcdab1b18f690
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2026-01-24 23:10:56 +0530

    Time: 148 ms (96.32%) | Memory: 100 MB (64.82%) - LeetSync

---
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+       sort(nums.begin(), nums.end());
+       int n=nums.size();
+       int mn=0;
+
+       for(int i=0; i<n/2; i++){
+        mn=max(nums[i]+nums[n-1-i], mn);
+       } 
+
+       return mn;
+    }
+};
