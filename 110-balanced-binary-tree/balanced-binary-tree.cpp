07e72ec6877276314ba015279eb5172b784cce3d
Surbhi Srivastava <105586968+surbhi-sri@users.noreply.github.com>
2026-02-08 23:49:23 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (20.68%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -6,29 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int check(TreeNode* root) {
-        if (!root)
+    
+    int solve(TreeNode* root, bool &ans) {
+        if(!root) return 0;
+        int left= solve(root->left, ans);
+        int right= solve(root->right, ans);
+
+        if(abs(left-right)>1){
+            ans=0;
             return 0;
+        }
 
-        int lh = check(root->left);
-        if (lh == -1)
-            return -1;
-
-        int rh = check(root->right);
-        if (rh == -1)
-            return -1;
-
-        if (abs(lh - rh) > 1)
-            return -1;
-
-        return 1 + max(lh, rh);
+        return max(left, right)+1;
     }
 
-    bool isBalanced(TreeNode* root) { return check(root) != -1; }
+
+    bool isBalanced(TreeNode* root) {
+        bool ans=1;
+        solve(root, ans);
+
+        return ans;
+    }
 };
